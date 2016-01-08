3b2246b52036c5e19ae57f95f94236ea2d78aa9e
kportillo <student@BLUC02MG1HLFH51.local>
2016-01-08 11:38:23 -0600

    initial commit

--- Multiply App 2/ViewController.swift
+++ Multiply App 2/ViewController.swift
@@ -8,17 +8,93 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+{ @IBOutlet weak var textFieldOne: UITextField!
+    @IBOutlet weak var textFieldTwo: UITextField!
+    @IBOutlet weak var labelOne: UILabel!
+    @IBOutlet weak var myImage: UIImageView!
+    @IBOutlet weak var segmentController: UISegmentedControl!
+
+
+
+    
+    
+    
+    
+    override func viewDidLoad()
+    {
+    super.viewDidLoad()
+      
+        
+        
+    }
+    
+    @IBAction func buttonPressed(sender: UIButton)
+   
+    { var inputOne = textFieldOne.text.toInt()
+        var inputTwo = textFieldTwo.text.toInt()
+        var answer = inputOne! * inputTwo!
+     
+        
+        if answer == 64
+        {myImage.image = UIImage (named:"toad")}
+        
+        else if answer % 2 == 1
+        {myImage.image = UIImage (named: "successbaby")}
+        
+         else if answer % 2 == 0
+        {myImage.image = UIImage (named: "feelit")}
+        
+        
+        
+        if (segmentController.selectedSegmentIndex) == 0
+        {answer = inputOne! + inputTwo! }
+        
+        
+        if (segmentController.selectedSegmentIndex) == 1
+        {answer = inputOne! - inputTwo!}
+        
+        if (segmentController.selectedSegmentIndex) == 2
+        {answer = inputOne! * inputTwo!}
+        
+        
+        if (segmentController.selectedSegmentIndex) == 3
+        {answer = inputOne! / inputTwo!}
+        
+
+               labelOne.text = "\(answer)" //makes interger variable into a string//
+        
+        
+        
+  
+        
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
